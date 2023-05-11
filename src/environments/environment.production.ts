baab96b4561177495c3222778c5e9ca09b981071
PEREYRA ESPECHE, Fabricio Daniel <fabriciopereyra@gmail.com>
2023-05-11 17:34:02 -0300

    railway10

--- src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  URL: 'https://portfoliobackendmodulo8-1-production.up.railway.app/',
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,6 @@
 export const environment = {
-  firebase: {
-    projectId: 'portfoliobackendmodulo8-1',
-    appId: '1:241638522862:web:d589b11e73fbbba06358ff',
-    storageBucket: 'portfoliobackendmodulo8-1.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyC4dcZpUHLOmszkD8t21zYkgEqRKtJZACI',
-    authDomain: 'portfoliobackendmodulo8-1.firebaseapp.com',
-    messagingSenderId: '241638522862',
-  },
-    production: false,
+  production: false,
 
-    //URL: 'http://localhost:8080/',
-    URL: 'https://portfoliobackendmodulo8-1.onrender.com/'
-
+  //URL: 'http://localhost:8080/',
+  URL: 'https://portfoliobackendmodulo8-1-production.up.railway.app/',
 };
-
